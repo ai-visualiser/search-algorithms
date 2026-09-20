55e4c29d99264fa79c741d86ab5938b8b8748587
Richard Wong <ricwtk@gmail.com>
2026-09-20 18:07:19 +0800

    add vue-flow and @formkit/drag-and-drop basic components

--- src/components/FrontierList.vue
+++ src/components/FrontierList.vue
@@ -1,5 +1,30 @@
 <script setup lang="ts">
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { ref } from 'vue';
+import { useDragAndDrop } from '@formkit/drag-and-drop/vue';
+const frontiers = [
+  {
+    state: "a",
+    path: "a-b-a",
+    cost: 10,
+    heuristic: 5,
+  },
+  {
+    state: "b",
+    path: "a-c-b",
+    cost: 5,
+    heuristic: 9,
+  },
+  {
+    state: "c",
+    path: "a-b-c",
+    cost: 8,
+    heuristic: 3,
+  },
+];
+const [frontierList, frontierItems] = useDragAndDrop(frontiers, {
+  dragHandle: ".drag-handle"
+});
 </script>
 
 <template>
@@ -8,6 +33,20 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
       <CardTitle>Frontier List</CardTitle>
     </CardHeader>
     <CardContent class="min-w-[200px] max-w-0.5">
+      <div class="flex flex-col gap-2" ref="frontierList">
+        <template v-for="item in frontierItems" :key="item">
+          <Card>
+            <CardHeader>
+              <CardTitle class="drag-handle">{{ item.state }}</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div>{{ item.path }}</div>
+              <div>{{ item.cost }}</div>
+              <div>{{ item.heuristic }}</div>
+            </CardContent>
+          </Card>
+        </template>
+      </div>
     </CardContent>
   </Card>
 </template>
--- src/components/TreeArea.vue
+++ src/components/TreeArea.vue
@@ -1,12 +1,31 @@
 <script setup lang="ts">
 import { Card, CardContent } from '@/components/ui/card';
+import type { Node, Edge } from '@vue-flow/core'
+import { VueFlow } from '@vue-flow/core'
 
+const nodes: Node[] = [
+  { id: 'node-1', position: { x: 0, y: 0 } },
+  { id: 'node-2', position: { x: 100, y: 100 } },
+]
+
+const edges: Edge[] = [
+  { id: 'edge-1', source: 'node-1', target: 'node-2' },
+]
 </script>
 
 <template>
   <Card>
-    <CardContent>
-
+    <CardContent class="flex flex-row grow">
+      <VueFlow :nodes="nodes" :edges="edges" class="bg-gray-50 rounded">
+      </VueFlow>
     </CardContent>
   </Card>
 </template>
+
+<style>
+/* import the necessary styles for Vue Flow to work */
+@import '@vue-flow/core/dist/style.css';
+
+/* import the default theme, this is optional but generally recommended */
+@import '@vue-flow/core/dist/theme-default.css';
+</style>
