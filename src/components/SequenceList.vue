b6c050be852c0b0868bdc97fe6f42f8e5b6be41f
Richard Wong <ricwtk@gmail.com>
2026-09-22 17:51:30 +0800

    update display of current step in sequence list

--- src/components/SequenceList.vue
+++ src/components/SequenceList.vue
@@ -1,15 +1,25 @@
 <script setup lang="ts">
 import { Button } from '@/components/ui/button';
+import { ref } from 'vue';
+
+const totalStepNumber = 20;
+const currentStep = ref(Math.floor(Math.random() * totalStepNumber) + 1);
 </script>
 
 <template>
   <div class="flex flex-col items-center p-2 overflow-y-auto">
-    <Button variant="outline" class="rounded-full" size="icon">
-      <span>2</span>
-    </Button>
-    <span class="text-border">|</span>
-    <Button variant="outline" class="rounded-full" size="icon">
-      <span>20</span>
-    </Button>
+    <template v-for="step in totalStepNumber" :key="step">
+      <Button variant="outline" class="rounded-full" size="icon"
+        :class="{
+          'bg-accent': step === currentStep,
+          'border-primary': step === currentStep,
+          'font-bold': step === currentStep
+        }"
+        @click="currentStep = step"
+      >
+        <span>{{ step }}</span>
+      </Button>
+      <span class="text-border" v-if="step !== totalStepNumber">|</span>
+    </template>
   </div>
 </template>
